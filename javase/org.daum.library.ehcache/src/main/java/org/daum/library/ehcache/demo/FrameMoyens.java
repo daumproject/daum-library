165016146110649d33b7d9a76020a8f951913d99
JeD <jedartois@gmail.com>
2012-05-21 14:13:14 +0200

    add ehcache

---
+++ javase/org.daum.library.ehcache/src/main/java/org/daum/library/ehcache/demo/FrameMoyens.java
@@ -0,0 +1,48 @@
+package org.daum.library.ehcache.demo;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
+/**
+ * Created by jed
+ * User: jedartois@gmail.com
+ * Date: 15/05/12
+ * Time: 10:26
+ */
+public class FrameMoyens extends JFrame {
+
+    private static final int FRAME_WIDTH = 300;
+    private static final int FRAME_HEIGHT = 600;
+    private JTable table;
+    String rowData[][] = { { "BLS", "RENNES", "LIBRE" },
+            { "FPT", "RENNES", "LIBRE" } };
+
+    Object columnNames[] = { "Type de Moyen", "Caserne", "Etat" };
+
+    public FrameMoyens(String nodeName) {
+
+        setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
+        setLayout(new BorderLayout());
+        setTitle(nodeName);
+        setVisible(true);
+
+        table = new JTable(rowData, columnNames);
+
+        JScrollPane scrollPane = new JScrollPane(table);
+        add(scrollPane, BorderLayout.CENTER);
+
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        pack();
+        setVisible(true);
+    }
+
+
+    public void update(String rowData[][]){
+        this.rowData = rowData;
+        DefaultTableModel model = new DefaultTableModel( rowData, columnNames);
+        table.setModel(model);
+    }
+
+
+}
