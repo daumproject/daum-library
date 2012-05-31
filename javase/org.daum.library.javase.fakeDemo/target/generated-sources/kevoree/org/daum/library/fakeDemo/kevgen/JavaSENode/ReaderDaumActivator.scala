dbd16dae2f3dc72fa4f2d079931aa0f2675be5b4
François Fouquet <fouquet.f@gmail.com>
2012-05-31 19:02:29 +0200

    f

--- javase/org.daum.library.javase.fakeDemo/target/generated-sources/kevoree/org/daum/library/fakeDemo/kevgen/JavaSENode/ReaderDaumActivator.scala
+++
@@ -1,9 +0,0 @@
-package org.daum.library.fakeDemo.kevgen.JavaSENode;
-import java.util.Hashtable
-import org.kevoree.api.service.core.handler.KevoreeModelHandlerService
-import org.kevoree.framework.KevoreeActor
-import org.kevoree.framework.KevoreeComponent
-import org.kevoree.framework._
-import org.daum.library.fakeDemo._
-class ReaderDaumActivator extends org.kevoree.framework.osgi.KevoreeComponentActivator {
-def callFactory() : KevoreeComponent = { ReaderDaumFactory.createComponentActor() } }
