fa401dbbc82dc8704c70ce1ff7b4027017b988a9
JeD <jedartois@gmail.com>
2012-07-02 10:25:13 +0200

    add tila k component

---
+++ javase/org.daum.library.javase.tila/src/main/java/org/daum/library/javase/tila/Tila.java
@@ -0,0 +1,35 @@
+package org.daum.library.javase.tila;
+
+import org.kevoree.annotation.*;
+import org.kevoree.framework.AbstractComponentType;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jed
+ * Date: 02/07/12
+ * Time: 10:22
+ * To change this template use File | Settings | File Templates.
+ */
+@Library(name = "JavaSE")
+@ComponentType
+public class Tila extends AbstractComponentType {
+
+    @Start
+    public void start()
+    {
+
+
+    }
+
+    @Stop
+    public void stop()
+    {
+
+    }
+
+    @Update
+    public void update()
+    {
+
+    }
+}
