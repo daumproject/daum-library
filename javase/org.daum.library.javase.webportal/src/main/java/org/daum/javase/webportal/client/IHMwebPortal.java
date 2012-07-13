5385afd4fed2363f770cac0270ea020fdba43b97
Pierre <despagnepierre@gmail.com>
2012-07-13 16:33:34 +0200

    maj webportal

---
+++ javase/org.daum.library.javase.webportal/src/main/java/org/daum/javase/webportal/client/IHMwebPortal.java
@@ -0,0 +1,22 @@
+package org.daum.javase.webportal.client;
+
+
+import com.google.gwt.core.client.EntryPoint;
+
+
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class IHMwebPortal implements EntryPoint {
+
+
+    /**
+     * This is the entry point method.
+     */
+    public void onModuleLoad() {
+        AuthentiForm authenificationForm = new AuthentiForm();
+        authenificationForm.init();
+    }
+}
+
