008235645914283e3683e6766457f4337bd64cf0
JeD <jedartois@gmail.com>
2012-09-18 13:27:45 +0200

    goto kevoree 1.8.4

---
+++ arduino/org.daum.library.arduino.sensors/src/main/java/org/daum/library/arduino/sensors/Balise.java
@@ -0,0 +1,11 @@
+package org.daum.library.arduino.sensors;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jed
+ * Date: 18/09/12
+ * Time: 09:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class Balise {
+}
