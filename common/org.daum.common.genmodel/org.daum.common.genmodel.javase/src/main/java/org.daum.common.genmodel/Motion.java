3dbc46edaaa75ea21dfa8c0933e003036ba0cdbf
JeD <jedartois@gmail.com>
2012-09-28 17:22:27 +0200

    add motion pojo

--- common/org.daum.common.genmodel/org.daum.common.genmodel.javase/src/main/java/org.daum.common.genmodel/Motion.java
+++ common/org.daum.common.genmodel/org.daum.common.genmodel.javase/src/main/java/org.daum.common.genmodel/Motion.java
@@ -8,4 +8,33 @@ package org.daum.common.genmodel;
  * To change this template use File | Settings | File Templates.
  */
 public class Motion {
+
+    float x ;
+    float y  ;
+    float z   ;
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
 }
