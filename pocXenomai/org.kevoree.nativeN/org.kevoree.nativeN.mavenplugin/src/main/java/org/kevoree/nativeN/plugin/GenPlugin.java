780639451c10a6c96e69a9d8771daf6bda5c3875
JeD <jedartois@gmail.com>
2012-09-28 17:22:39 +0200

    refactoring

---
+++ pocXenomai/org.kevoree.nativeN/org.kevoree.nativeN.mavenplugin/src/main/java/org/kevoree/nativeN/plugin/GenPlugin.java
@@ -0,0 +1,16 @@
+package org.kevoree.nativeN.plugin;
+
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+
+public class GenPlugin extends AbstractMojo
+{
+
+
+    public void execute()   throws MojoExecutionException
+    {
+
+    }
+}
